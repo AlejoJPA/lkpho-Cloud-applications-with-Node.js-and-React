c2391b07b0f3aeeb0d46438873a94d9e1338793d
Alejo <alejojpa2@gmail.com>
2024-10-22 17:02:05 +0200

    today.js was modified to work with index_with_require

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,16 @@
  // Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
     // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+    //let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"}); //"en-US" USA forman , en: Enlish, date: AM and PM
+
+    // Get the current date and time in the timezone "Europe/Berlin"
+    let frankfurtTime = new Date().toLocaleString("de-DE", {timeZone: "Europe/Berlin" }); // German format: "de-DE"
+    //return frankfurtTime;
+    //console.log(`Frankfurt time: ${frankfurtTime}`); 
+    //return aestTime; // Return the formatted date and time
+
+    /*NOTE: Wen used by 'index-with-require' the return of today must be the plain function without asigment operation. 
+    The variable frankfurtTime can be canceled as it has not impact */
+    return new Date() // does not work: Date().toLocaleString("de-DE", {timeZone: "Europe/Berlin" }); 
+
 };
